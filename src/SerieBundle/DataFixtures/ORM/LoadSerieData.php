cad88e1a6c84e637eea4f00488e46816a7196737
zeecho <zeecho@users.noreply.github.com>
2016-03-20 21:43:11 +0100

    Adding birthDate to User Form

---
+++ src/SerieBundle/DataFixtures/ORM/LoadSerieData.php
@@ -0,0 +1,23 @@
+<?php // src/AppBundle/DataFixtures/ORM/LoadSerieData.php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+use Symfony\Component\Validator\Constraints\Date;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword('test');
+        $userAdmin->setEmail("arnsie@a.com");
+        $userAdmin->setBirthDate(new Date());
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
